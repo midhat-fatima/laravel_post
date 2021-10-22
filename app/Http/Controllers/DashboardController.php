5ce0281283b875afd745fc533b014d6cc7e9eef8
midhat-fatima <smidhatfatimaa@gmail.com>
2021-10-23 01:49:55 +0500

    post

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\PostLiked;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Mail;
+
+class DashboardController extends Controller
+{
+    public function __construct() {
+
+        $this->middleware(['auth']);
+    } 
+    
+    public function index() {
+        
+        $user = auth()->user();
+
+        Mail::to($user)->send(new PostLiked());
+
+        return view('dashboard');
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Like;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body',
+    ];
+
+    public function likedBy(User $user) {
+        return $this->likes->contains('user_id', $user->id);
+    }
+
+    // public function ownedBy(User $user) {
+    //     return $user->id === $this->user->id;
+    // }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
+}
